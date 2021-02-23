8c83e2a79848d4691247f80a9873050c85a9acc1
Eli Knebel <eliknebel@gmail.com>
2021-02-22 21:36:48 -0500

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lti_1p3.MixProject do
   def project do
     [
       app: :lti_1p3,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
