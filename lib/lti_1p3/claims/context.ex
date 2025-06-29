bffda2860a13bb77b63708f85cb51a957381afdc
Eli Knebel <eliknebel@cmu.edu>
2025-06-29 14:03:40 -0400

    fix context and platform instance claims

--- lib/lti_1p3/claims/context.ex
+++ lib/lti_1p3/claims/context.ex
@@ -5,13 +5,17 @@ defmodule Lti_1p3.Claims.Context do
   https://www.imsglobal.org/spec/lti/v1p3#context-claim
   """
 
+  @enforce_keys [:id]
+
   defstruct [
+    :id,
     :label,
     :title,
     :type
   ]
 
   @type t() :: %__MODULE__{
+          id: String.t(),
           label: String.t(),
           title: String.t(),
           type: String.t()
@@ -22,8 +26,9 @@ defmodule Lti_1p3.Claims.Context do
   @doc """
   Create a new context claim.
   """
-  def context(opts \\ []) do
+  def context(id, opts \\ []) do
     %__MODULE__{
+      id: id,
       label: Keyword.get(opts, :label),
       title: Keyword.get(opts, :title),
       type: Keyword.get(opts, :type)
@@ -35,11 +40,13 @@ defimpl Lti_1p3.Claims.Claim, for: Lti_1p3.Claims.Context do
   def get_key(_), do: Lti_1p3.Claims.Context.key()
 
   def get_value(%Lti_1p3.Claims.Context{
+        id: id,
         label: label,
         title: title,
         type: type
       }) do
     %{
+      "id" => id,
       "label" => label,
       "title" => title,
       "type" => type
